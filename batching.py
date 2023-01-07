1bdb1079c1e6267a5d17c40c231fad1b8f912ad8
tomMcGrath <thomas.m.mcgrath@gmail.com>
2023-01-07 12:13:16 +0000

    Fix off-by-one error in processing.

--- batching.py
+++ batching.py
@@ -2,25 +2,27 @@ import collections
 import char_tokeniser
 
 
-def process_sequence(tokeniser, seq, subseq_len):
-    """Process a sequence into batches of tokens and targets."""
-    if not seq:
-        return None
-    
-    tokens = tokeniser.tokenise(seq)
-    targets = [tokens[i+1] for i in range(len(tokens) - 1)]
-    split_tokens = char_tokeniser.split_into_subseqs(tokens, subseq_len)
-    split_targets = char_tokeniser.split_into_subseqs(targets, subseq_len)
-    data = [{'tokens': split_tokens[i], 'targets': split_targets[i]} for i in range(len(split_tokens))]
-    return data
+def make_processor(tokeniser, subseq_len):
+    def process_sequence(seq):
+        """Process a sequence into batches of tokens and targets."""
+        if not seq:
+            return None
+
+        tokens = tokeniser.tokenise(seq)
+        targets = [tokens[i+1] for i in range(len(tokens) - 1)]
+        split_tokens = char_tokeniser.split_into_subseqs(tokens, subseq_len)
+        split_targets = char_tokeniser.split_into_subseqs(targets, subseq_len)
+        data = [{'tokens': split_tokens[i], 'targets': split_targets[i]} for i in range(len(split_tokens))]
+        return data
+    return process_sequence
+
 
 class DataSource:
     
-    def __init__(self, data_iterator, process_fn, seq_len):
+    def __init__(self, data_iterator, process_fn):
         """Initialise the DataSource."""
         self.data_iterator = data_iterator
         self.process_fn = process_fn
-        self.seq_len = seq_len
         self._data_deque = collections.deque()
         
     def _refill(self):
@@ -40,6 +42,9 @@ class DataSource:
             next_datum = self._data_deque.popleft()
         except IndexError:
             self._refill()
+            next_datum = self._data_deque.popleft()
             
         if not self._data_deque:  # prefer refilling before IndexError
             self._refill()
+            
+        return next_datum
