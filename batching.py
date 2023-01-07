304c48ef036235b97e31c888c082aa39556ccd85
tomMcGrath <thomas.m.mcgrath@gmail.com>
2023-01-07 12:17:46 +0000

    Add pad/discard to processor.

--- batching.py
+++ batching.py
@@ -2,7 +2,7 @@ import collections
 import char_tokeniser
 
 
-def make_processor(tokeniser, subseq_len):
+def make_processor(tokeniser, subseq_len, discard_last=False):
     def process_sequence(seq):
         """Process a sequence into batches of tokens and targets."""
         if not seq:
@@ -12,6 +12,15 @@ def make_processor(tokeniser, subseq_len):
         targets = [tokens[i+1] for i in range(len(tokens) - 1)]
         split_tokens = char_tokeniser.split_into_subseqs(tokens[:-1], subseq_len)
         split_targets = char_tokeniser.split_into_subseqs(targets, subseq_len)
+
+        # Pad or discard
+        if discard_last:
+            split_tokens = split_tokens[:-1]
+            split_targets = split_targets[:-1]
+        else:
+            split_tokens[-1] = tokeniser.pad(split_tokens[-1], subseq_len)
+            split_targets[-1] = tokeniser.pad(split_targets[-1], subseq_len)
+
         data = [{'tokens': split_tokens[i], 'targets': split_targets[i]} for i in range(len(split_tokens))]
         return data
     return process_sequence
